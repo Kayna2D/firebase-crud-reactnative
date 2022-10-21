d369e829854fe241f4d3ae00ba3ca18a7fa35ea1
unknown <you@example.com>
2022-10-21 16:23:12 -0300

    form conectado ao bd

--- src/paginas/cadastro/index.js
+++ src/paginas/cadastro/index.js
@@ -1,12 +1,36 @@
 import { Component } from "react";
 import { Text,View, Button, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { TextInput } from "react-native";
+import firebase from "../../firebase/firebaseConnection";
 
+export default function Cadastro() {
 
-export default class Cadastro extends React.Component {
-    render() {
+      const [nome, setNome] = useState("");
+      const [nota1, setNota1] = useState("");
+      const [nota2, setNota2] = useState("");
+      const [nota3, setNota3] = useState("");
+      const [imagem, setImagem] = useState("");
+
+      async function Cadastrar() {
+
+        const Alunos = await firebase.database().ref('Alunos');
+
+        const chave = Alunos.push().key;
+
+        Alunos.child(chave).set({
+          Nome: nome,
+          Nota1: nota1,
+          Nota2: nota2,
+          Nota3: nota3,
+          Imagem: imagem,
+        })
+
+      }
+
       return (
+        
+
         <View style={styles.container}>
           <View>
             <Text style={styles.header}>Cadastro</Text>
@@ -16,12 +40,14 @@ export default class Cadastro extends React.Component {
                     style={styles.textInput}
                     placeholder="Nome"
                     maxLength={50}
+                    onChangeText={(texto) => setNome(texto)}
                     />
                 </View>
                 <View style={styles.inputContainer}>
                     <TextInput
                     style={styles.textInput}
                     placeholder="Nota 1"
+                    onChangeText={(texto) => setNota1(texto)}
                     maxLength={5}
                     />
                 </View>
@@ -30,6 +56,7 @@ export default class Cadastro extends React.Component {
                     style={styles.textInput}
                     placeholder="Nota 2"
                     maxLength={5}
+                    onChangeText={(texto) => setNota2(texto)}
                     />
                 </View>
                 <View style={styles.inputContainer}>
@@ -37,6 +64,7 @@ export default class Cadastro extends React.Component {
                     style={styles.textInput}
                     placeholder="Nota 3"
                     maxLength={5}
+                    onChangeText={(texto) => setNota3(texto)}
                     />
                 </View>
                 <View style={styles.inputContainer}>
@@ -44,11 +72,13 @@ export default class Cadastro extends React.Component {
                     style={styles.textInput}
                     placeholder="Foto de Perfil"
                     maxLength={100}
+                    onChangeText={(texto) => setImagem(texto)}
                     />
                 </View>
                 <View style={styles.inputContainer}>
-                    <TouchableOpacity
+                    <TouchableOpacity 
                         style={styles.saveButton}
+                        onPress={Cadastrar}
                     >
                         <Text style={styles.saveButtonText}>Cadastrar</Text>
                     </TouchableOpacity>
@@ -58,7 +88,7 @@ export default class Cadastro extends React.Component {
         </View>
       );
     }
-  }
+  
   
   const styles = StyleSheet.create({
     container: {
